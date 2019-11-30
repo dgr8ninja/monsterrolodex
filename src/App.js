fda28c150363e37c9210cf865dc4d39469ae87f5
dgr8ninja <dr4602@gmail.com>
2019-11-30 09:56:32 -0600

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
+
+
 
 class App extends Component {
   constructor(){
@@ -7,33 +10,32 @@ class App extends Component {
 
     this.state = {
 
-      monsters: [
-        {
-        name: 'Frankenstein',
-        id: 'mon1'
-      },
-      {
-        name: 'Dracula',
-        id: 'mon2',
-      },
-      {
-        name: 'Zombie',
-        id: 'mon3'
-      }
-
-    ]
+      monsters: [],
+      searchField: ''
 
     };
 
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return(
       
       <div className="App">
-            {
-  this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
-            }
-          </div>
+        <input 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={e => this.setState({ searchField: e.target.value })}
+      />
+        <CardList monsters={this.state.monsters}>
+          
+        </CardList>          
+       </div>
 
     );
   }
