165624a606426b703466942002d8aad66b4b404a
dgr8ninja <dr4602@gmail.com>
2019-11-30 22:30:12 -0600

    Updates

---
+++ precache-manifest.8141acfa0b73162b15039834f9c8bf5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de87304cd9a6ab00fedef0ad98b467d",
+    "url": "/monsterrolodex/index.html"
+  },
+  {
+    "revision": "10f34a88fb2ed170058c",
+    "url": "/monsterrolodex/static/css/main.bb36c45d.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/monsterrolodex/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "10f34a88fb2ed170058c",
+    "url": "/monsterrolodex/static/js/main.0b0ac16c.chunk.js"
+  },
+  {
+    "revision": "0c8d7af5ab46cb11514e",
+    "url": "/monsterrolodex/static/js/runtime-main.60c47ba8.js"
+  }
+]);
